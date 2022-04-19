5ab2e844172d23e7690e0784e7b0e18779988659
Michael Gregory <michael.gregory513@gmail.com>
2022-04-19 10:14:09 -0400

    Initial Commit

---
+++ DungeonDice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DungeonDice
+//
+//  Created by Michael Gregory on 4/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
