ef41951ea09b418adfe4f4a0e18c833c108dff34
Michael Gregory <michael.gregory513@gmail.com>
2022-04-19 12:06:24 -0400

    trying to do this exercise

--- DungeonDice/ViewController.swift
+++ DungeonDice/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	@IBOutlet weak var diceLabel: UILabel!
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
-
+	@IBAction func rollDicePress(_ sender: UIButton) {
+		print(sender.tag)
+		var diceRollNumber = 0
+		var diceType = 0
+		
+	}
+	
 
 }
 
